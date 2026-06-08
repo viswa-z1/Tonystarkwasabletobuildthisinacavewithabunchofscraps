748772e2d30e88ab6e9253abd0e969790683ce0a
Viswa <viswa3031@gmail.com>
2026-06-08 23:36:37 +0530

    Frontend - Livekit room

---
+++ apps/mobile/hooks/useLiveKitRoom.ts
@@ -0,0 +1,6 @@
+// TODO: implement with @livekit/react-native
+export function useLiveKitRoom(token: string | null, url: string | null) {
+  void token
+  void url
+  return { connected: false, disconnect: () => {} }
+}
